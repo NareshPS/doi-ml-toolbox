d9dd9118d8c574a583bf034f1c99ceb21825ce06
ludak <ludak@ludak.me>
2023-09-01 22:08:58 +0700

    Added support for a subset dataset run

--- torch/toolbox/data_download.py
+++ torch/toolbox/data_download.py
@@ -1,17 +1,14 @@
-
 import os
 import requests
 import zipfile
 
 from pathlib import Path
 
-def download_data(
-    source: str,
-    destination: str,
-    remove_source: bool = True
-) -> Path:
+
+def download_data(source: str, destination: str, remove_source: bool = True) -> Path:
+    # sourcery skip: extract-method
     """Downloads zipped dataset from source and unzips to the destination directory.
-    
+
     Usage Example:
         download_data(
             source='https://github.com/mrdbourke/pytorch-deep-learning/raw/main/data/pizza_steak_sushi.zip',
@@ -19,29 +16,30 @@ def download_data(
         )
     """
     # 1. Setup path to data folder
-    data_path = Path(os.path.expanduser('~/.datasets/'))
+    data_path = Path(os.path.expanduser("~/.datasets/"))
     image_path = data_path / destination
 
-    # If the image folder doesn't exist, download it and prepare it... 
+    # If the image folder doesn't exist, download it and prepare it...
     if image_path.is_dir():
         print(f"[INFO] {image_path} directory exists. Skipping download.")
     else:
         print(f"[INFO] Did not find {image_path} directory, creating one...")
         image_path.mkdir(parents=True, exist_ok=True)
-        
+
         # 2. Download the source file
         remote_file_name = Path(source).name
         with open(data_path / remote_file_name, "wb") as f:
             request = requests.get(source)
-            print(f'[INFO] Downloading {remote_file_name} from {source}')
+            print(f"[INFO] Downloading {remote_file_name} from {source}")
             f.write(request.content)
 
         # 3. Unzip the source file into the destination directory
         with zipfile.ZipFile(data_path / remote_file_name, "r") as zip_ref:
-            print(f"[INFO] Unzipping {remote_file_name} data...") 
+            print(f"[INFO] Unzipping {remote_file_name} data...")
             zip_ref.extractall(image_path)
-        
+
         # 4. Cleanup the downloaded file.
-        if remove_source: os.remove(data_path / remote_file_name)
-    
+        if remove_source:
+            os.remove(data_path / remote_file_name)
+
     return image_path
--- torch/toolbox/data_setup.py
+++ torch/toolbox/data_setup.py
@@ -1,18 +1,19 @@
-
 import os
 
 from pathlib import Path
 from torchvision import datasets, transforms
-from torch.utils.data import DataLoader
+from torch.utils.data import DataLoader, Subset
 
 WORKERS = os.cpu_count()
+
 
 def create_dataloaders(
     train_path: Path,
     test_path: Path,
     transform: transforms.Compose,
     batch_size: int,
-    workers: int = WORKERS
+    workers: int = WORKERS,
+    subset_size: int = None,
 ):
     """Creates train and test DataLoaders
 
@@ -24,29 +25,22 @@ def create_dataloaders(
         transform: torchvision transform to apply over the training and testing data.
         batch_size: The number of samples in each batch of DataLoaders.
         workers: The number of workers per DataLoader to read the data.
+        subset_size: The number of elements to load from the datasets.
     """
-    # 1.  Create Datasets using datasets.ImageFolder
-    train_data = datasets.ImageFolder(root=train_path, transform=transform)
-    test_data = datasets.ImageFolder(root=test_path, transform=transform)
+    # 1.  Create Datasets
+    train_data = create_dataset(
+        data_path=train_path, transform=transform, subset_size=subset_size, name="Train"
+    )
+    test_data = create_dataset(
+        data_path=test_path, transform=transform, subset_size=subset_size, name="Test"
+    )
 
     # 2. Get the classes in the input data.
-    class_names = train_data.classes
-    class_dict = train_data.class_to_idx
+    class_names = (
+        train_data.classes if subset_size is None else train_data.dataset.classes
+    )
 
-    # 3. Print basic data information
-    print('Training Set')
-    print('------------')
-    print(f'{train_data}')
-    print(f'Classes: {train_data.classes}')
-    print(f'Class Dictionary: {train_data.class_to_idx}')
-
-    print('\nTest Set')
-    print('------------')
-    print(f'{test_data}')
-    print(f'Classes: {test_data.classes}')
-    print(f'Class Dictionary: {test_data.class_to_idx}')
-
-    # 4. Batch the data using DataLoaders.
+    # 3. Batch the data using DataLoaders.
     train_dataloader = DataLoader(
         dataset=train_data,
         batch_size=batch_size,
@@ -64,3 +58,36 @@ def create_dataloaders(
     )
 
     return train_dataloader, test_dataloader, class_names
+
+
+def create_dataset(
+    data_path: Path,
+    transform: transforms.Compose,
+    subset_size: int = None,
+    name: str = "Data",
+):
+    """Creates a torch dataset from image folder
+
+    Arguments:
+        data_path: Path to the data directory.
+        transform: torchvision transform to apply over the training and testing data.
+        subset_size: The number of elements to load from the dataset.
+    """
+    # 1. Create a dataset to load the data from the data path
+    dataset = datasets.ImageFolder(root=data_path, transform=transform)
+    dataset_summary(name, dataset)
+
+    # 2. Pick subset of data, if required
+    if subset_size is not None:
+        print(f"\n[WARN] Using Subset Size: {subset_size}")
+        dataset = Subset(dataset, list(range(subset_size)))
+
+    return dataset
+
+
+def dataset_summary(name, dataset):
+    print(f"\n{name} Set")
+    print("------------")
+    print(f"{dataset}")
+    print(f"Classes: {dataset.classes}")
+    print(f"Class Dictionary: {dataset.class_to_idx}")
